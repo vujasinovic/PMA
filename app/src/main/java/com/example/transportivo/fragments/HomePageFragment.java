ec37f9a68c2214f8f02ea59c3f66f6c01ea88b76
Pavle Jankovic <pavle.gp@gmail.com>
2020-05-10 16:27:45 +0200

    Rename Home Page Fragment to Home Fragment

--- app/src/main/java/com/example/transportivo/fragments/HomePageFragment.java
+++ app/src/main/java/com/example/transportivo/fragments/HomePageFragment.java
@@ -18,7 +18,7 @@ public class HomePageFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.home_page_fragment, container, false);
+        return inflater.inflate(R.layout.home_fragment, container, false);
     }
 
     @Override
