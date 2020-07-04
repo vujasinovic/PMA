40d07b71104bd4e4b3f870b593058fd621adbc2d
Jovana Grabež <44621211+jovanagrabez@users.noreply.github.com>
2020-07-04 16:50:59 +0200

    change password fixed (#19)

--- app/src/main/java/com/example/transportivo/fragments/SettingsFragment.java
+++ app/src/main/java/com/example/transportivo/fragments/SettingsFragment.java
@@ -1,18 +1,23 @@
 package com.example.transportivo.fragments;
 
 import android.transition.Visibility;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.transportivo.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import lombok.NonNull;
 
 public class SettingsFragment extends BaseFragment {
     private FirebaseUser user;
@@ -28,17 +33,20 @@ public class SettingsFragment extends BaseFragment {
     protected View initializeView(View view) {
 
         TextInputLayout textInputLayout = view.findViewById(R.id.editPassword);
+        TextInputLayout odlPassword = view.findViewById(R.id.editOldPassword);
         Button saveButton = view.findViewById(R.id.savePassword);
         saveButton.setVisibility(View.GONE);
         textInputLayout.setVisibility(View.GONE);
+        odlPassword.setVisibility(View.GONE);
         TextView changePassword = view.findViewById(R.id.changePassword);
         changePassword.setOnClickListener(v -> {
             textInputLayout.setVisibility(View.VISIBLE);
             saveButton.setVisibility(View.VISIBLE);
+            odlPassword.setVisibility(View.VISIBLE);
 
         });
 
-        saveButton.setOnClickListener(v->{
+        saveButton.setOnClickListener(v -> {
             updatePassword(view);
         });
 
@@ -49,19 +57,32 @@ public class SettingsFragment extends BaseFragment {
 
         TextInputEditText password = view.findViewById(R.id.newPassword);
         TextInputLayout textInputLayout = view.findViewById(R.id.editPassword);
+        TextInputLayout odlPassword = view.findViewById(R.id.editOldPassword);
+        TextInputEditText old = view.findViewById(R.id.oldPassword);
+
+
         Button saveButton = view.findViewById(R.id.savePassword);
 
+        AuthCredential credential = EmailAuthProvider
+                .getCredential(user.getEmail(), old.getText().toString());
 
-
-        user.updatePassword(password.getText().toString()).addOnCompleteListener(task -> {
-            if (task.isSuccessful()) {
-                saveButton.setVisibility(View.GONE);
-                textInputLayout.setVisibility(View.GONE);
-                System.out.println("User password updated.");
-            }
-        });
-
-
+        user.reauthenticate(credential)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        user.updatePassword(password.getText().toString()).addOnCompleteListener(task1 -> {
+                            if (task1.isSuccessful()) {
+                                saveButton.setVisibility(View.GONE);
+                                textInputLayout.setVisibility(View.GONE);
+                                odlPassword.setVisibility(View.GONE);
+                                Log.d("CHANGE PASSWORD", "Password updated");
+                            } else {
+                                Log.d("CHANGE PASSWORD", "Error password not updated");
+                            }
+                        });
+                    } else {
+                        Log.d("CHANGE PASSWORD", "Error auth failed");
+                    }
+                });
 
 
     }
